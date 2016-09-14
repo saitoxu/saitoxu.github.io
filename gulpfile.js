b677502b35b6b2f5f823b1c33e344f5c3ad77483
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-09-15 00:36:57 +0900

    change layout

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),
+    plumber = require('gulp-plumber'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
+
+gulp.task('default', function() {
+    gulp.watch(['less/**/*.less'], ['less']);
+})
+
+gulp.task('less', function() {
+    gulp.src('less/**/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('cssmin', function() {
+    gulp.src('css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
+});
