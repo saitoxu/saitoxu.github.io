708b2b4c94c8e6591e0da4974097b8b626e59f06
Yosuke Saito <yosuke.saito1202@gmail.com>
2016-12-06 10:01:41 +0900

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,22 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('default', function() {
-    gulp.watch(['less/**/*.less'], ['less']);
+  gulp.watch(['less/**/*.less'], ['less']);
+  gulp.watch(['css/*.css'], ['cssmin']);
 })
 
 gulp.task('less', function() {
-    gulp.src('less/**/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(gulp.dest('./css'));
+  gulp.src('less/**/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('cssmin', function() {
-    gulp.src('css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./css'));
+  gulp.src('css/*.css')
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'));
 });
