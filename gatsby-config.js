c305ca85481ededbd524e017f02052588b1eb32a
Yosuke Saito <yosuke.saito1202@gmail.com>
2020-01-18 00:56:03 +0900

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-37617637-3`,
       },
     },
     `gatsby-plugin-feed`,
