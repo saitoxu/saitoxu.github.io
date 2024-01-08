bfddf69cd2a92934b939ef2fc52580eeef536719
Yosuke Saito <yosuke.saito1202@gmail.com>
2024-01-08 18:36:48 +0900

    update images

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default async function NotFound() {
   return (
     <main>
-      <div className="py-24">
+      <div className="py-[20vmin]">
         <h1 className="leading-tight text-4xl font-semibold mb-4 mt-6 pb-2 text-center">
           404 Not Found 🙈
         </h1>
